9f6f861a828a80420c54b367b94801e1d844e57f
Emir-Karaman <75396000+Emir-Karaman@users.noreply.github.com>
2023-05-25 11:25:44 +0300

    Update signup.php

--- signup.php
+++ signup.php
@@ -6,18 +6,19 @@ include "config.php";
 $newuser_username = $_POST["username"];
 $newuser_password = $_POST["upassword"];
 
-
+if (empty($newuser_username) || empty($newuser_password)) {
+    echo "Kullanıcı adı ve şifre girilmesi gerekmektedir.";
+} else {
 
 $islem = $db -> query("INSERT INTO users (username,password) VALUES ('".$newuser_username."', '".$newuser_password."')");
 
 if ($islem == false)
 {
     echo("Error description: " . $db -> error);
-}
-else
-{
-    echo "Kayıt başarılı!<br>";
-    echo("Giriş yapmak için <a href='login_form.php'>tıklayınız.</a>");
+} else {
+        echo "Kayıt başarılı!<br>";
+        echo("Giriş yapmak için <a href='login_form.php'>tıklayınız.</a>");
+    }
 }
 
 
